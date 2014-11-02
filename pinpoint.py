fd6af1b8bc437c75394793e0258b59cc19d28542
Marcis <marcis.veveris@outlook.com>
2014-11-02 17:31:29 +0200

    Add bearer_token_credentials

--- pinpoint.py
+++ pinpoint.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask, request
+import base64
+from urllib import quote_plus
 
 
-APP_KEY = os.environ['APP_KEY']
-APP_SECRET = os.environ['APP_SECRET']
-APP_SECRET_KEY = os.environ['APP_SECRET_KEY']
+CONSUMER_KEY = os.environ['APP_KEY']
+CONSUMER_SECRET = os.environ['APP_SECRET']
+CONSUMER_SECRET_KEY = os.environ['APP_SECRET_KEY']
 
 
 app = Flask(__name__)
@@ -12,13 +14,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello'
+    bearer_token_credentials = base64_encode(':'.join([url_encode(CONSUMER_KEY), url_encode(CONSUMER_SECRET)]))
+    
+    return bearer_token_credentials
 
 
 @app.route('/callback')
 def callback():
     print(request)
     return 'test'
+
+
+def base64_encode(string):
+    return base64.urlsafe_b64encode(string)
+
+
+def url_encode(string):
+    return quote_plus(string)
 
 
 if __name__=='__main__':
