4831ca7de34376a47c9b53adfb99de0f3d3f9d1a
Marcis <marcis.veveris@outlook.com>
2014-11-02 18:25:36 +0200

    Add status retrieval

--- pinpoint.py
+++ pinpoint.py
@@ -1,6 +1,10 @@
 import os
-from flask import Flask, request
 import base64
+
+import requests
+import json
+
+from flask import Flask, request
 from urllib import quote_plus
 
 
@@ -14,15 +18,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    bearer_token_credentials = base64_encode(':'.join([url_encode(CONSUMER_KEY), url_encode(CONSUMER_SECRET)]))
+    bearer_token_credentials = create_bearer_credentials(CONSUMER_KEY, CONSUMER_SECRET)
+    bearer_token = get_bearer_token(bearer_token_credentials)
     
-    return bearer_token_credentials
+    resp = requests.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=1', headers={
+        'Authorization': 'Bearer {}'.format(bearer_token),
+        })
+
+    return json.dumps(resp.json(), indent=4)
+
+def create_bearer_credentials(consumer_key, consumer_secret):
+    return base64_encode(':'.join([url_encode(consumer_key), url_encode(consumer_secret)]))
 
 
-@app.route('/callback')
-def callback():
-    print(request)
-    return 'test'
+def get_bearer_token(credentials):
+    response = requests.post('https://api.twitter.com/oauth2/token', headers={
+        'Authorization': 'Basic {}'.format(credentials), 
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+    data={'grant_type': 'client_credentials'},
+    )
+    auth_data_dict = response.json()
+    assert auth_data_dict['token_type'] == 'bearer'
+    return auth_data_dict['access_token']
 
 
 def base64_encode(string):
